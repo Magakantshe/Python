40c1144734919c244b4cf49f86cb6a4dd9aa00e9
Magakantshe <133545246+Magakantshe@users.noreply.github.com>
2023-05-31 17:58:13 +0200

    Add files via upload

---
+++ survey_scale.sql
@@ -0,0 +1,57 @@
+CREATE DATABASE  IF NOT EXISTS `survey` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `survey`;
+-- MySQL dump 10.13  Distrib 5.5.16, for Win32 (x86)
+--
+-- Host: localhost    Database: survey
+-- ------------------------------------------------------
+-- Server version	5.5.62
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `scale`
+--
+
+DROP TABLE IF EXISTS `scale`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `scale` (
+  `contactnumber` varchar(10) NOT NULL,
+  `eat` int(4) NOT NULL,
+  `movie` int(4) NOT NULL,
+  `tv` int(4) NOT NULL,
+  `radio` int(4) NOT NULL,
+  PRIMARY KEY (`contactnumber`),
+  CONSTRAINT `scale_ibfk_1` FOREIGN KEY (`contactnumber`) REFERENCES `students` (`contactnumber`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `scale`
+--
+
+LOCK TABLES `scale` WRITE;
+/*!40000 ALTER TABLE `scale` DISABLE KEYS */;
+INSERT INTO `scale` VALUES ('0112234456',0,1,2,3),('0113456654',4,4,4,4),('0345567789',0,2,2,2),('0832214456',1,1,2,3);
+/*!40000 ALTER TABLE `scale` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-05-29 21:38:20
